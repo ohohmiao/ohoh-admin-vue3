09494767df5b8af266b87ca0a0e380dc7e56edb2
ohohmiao <245584101@qq.com>
2024-12-07 12:03:18 +0800

    Add：bpmn.js流程设计器toolbar

--- ohoh-admin-client/src/components/BpmnDesign/components/Toolbar/ScaleTools.vue
+++ ohoh-admin-client/src/components/BpmnDesign/components/Toolbar/ScaleTools.vue
@@ -17,14 +17,36 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import LucideIcon from "@/components/LucideIcon/index.vue";
+import type Modeler from "bpmn-js/lib/Modeler";
+import type Canvas from "diagram-js/lib/core/Canvas";
+import { CanvasEvent } from "diagram-js/lib/core/EventBus";
+import EventEmitter from "@/components/BpmnDesign/utils/eventEmitter";
 
 const currentScale = ref(1);
+let canvas: Canvas | null = null;
 
-const zoomOut = () => {};
+EventEmitter.on("modeler-init", (modeler: Modeler) => {
+	try {
+		canvas = modeler.get<Canvas>("canvas");
+		currentScale.value = canvas.zoom();
+	} finally {
+		modeler.on("canvas.viewbox.changed", ({ viewbox }: CanvasEvent) => {
+			currentScale.value = viewbox.scale;
+		});
+	}
+});
 
-const zoomReset = (param: string) => {
-	console.info(param);
+const zoomOut = (newScale?: number) => {
+	currentScale.value = newScale || Math.floor(currentScale.value * 100 - 0.1 * 100) / 100;
+	zoomReset(currentScale.value);
 };
 
-const zoomIn = () => {};
+const zoomReset = (newScale: any) => {
+	canvas && canvas.zoom(newScale, newScale === "fit-viewport" ? undefined : { x: 0, y: 0 });
+};
+
+const zoomIn = (newScale?: number) => {
+	currentScale.value = newScale || Math.floor(currentScale.value * 100 + 0.1 * 100) / 100;
+	zoomReset(currentScale.value);
+};
 </script>
